422f1c637bcfd303686a0f6735cfad31f2548e9a
NonokEE <shshrdl@naver.com>
2026-08-25 23:47:16 +0900

    fix: FirebaseAuthRequest에 @JsonProperty('id_token') 추가
    - Android 클라이언트가 snake_case(id_token)로 보낼 때 파싱 실패 문제 해결
    - Kotlin DTO는 camelCase지만 Jackson이 snake_case도 인식하도록 어노테이션 추가

--- src/main/kotlin/com/sesac/speechapp/dto/FirebaseAuthRequest.kt
+++ src/main/kotlin/com/sesac/speechapp/dto/FirebaseAuthRequest.kt
@@ -1,6 +1,9 @@
 package com.sesac.speechapp.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 // POST /api/v1/auth/firebase 요청
 class FirebaseAuthRequest(
+    @JsonProperty("id_token")
     val idToken: String
 )
